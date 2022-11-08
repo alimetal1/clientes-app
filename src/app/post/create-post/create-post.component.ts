33e69057c1fcf7577940597e494365f88529e299
AliMaster <javierapolet@gmail.com>
2022-11-07 22:21:05 -0300

    update

---
+++ src/app/post/create-post/create-post.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.css'],
+})
+export class CreatePostComponent implements OnInit {
+  
+  constructor(
+   
+  ) {
+   
+    };
+    
+    
+
+  ngOnInit(): void {}
+}
